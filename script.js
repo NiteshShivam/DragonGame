3796566c96d52f27ae65fe8038cac824ed59057e
NiteshShivam <niteshyadavand2000nitesh@gmail.com>
2024-05-24 12:36:20 +0530

    final update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+score = 0;
+cross = true;
+
+audio = new Audio('music.mp3');
+audiogo = new Audio('gameover.mp3');
+setTimeout(()=>{
+ audio.play();
+},1000);
+
 document.onkeydown  = function(e)
 {
     console.log("Key code is : ",e.keyCode);
@@ -7,6 +16,16 @@ document.onkeydown  = function(e)
         setTimeout(()=>{
             dino.classList.remove('animateDino')
         },700);
+    }
+    if(e.keyCode==39){
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112+"px";
+    }
+    if(e.keyCode==37){
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112)+"px";
     }
 }
 
@@ -23,9 +42,36 @@ setInterval(()=>{
 
     offsetX = Math.abs(dx-ox);
     offsetY = Math.abs(dy-oy);
-    if(offsetX<93 && offsetY<52){
-        gameOver.style.visibility = 'visible';
+    if(offsetX<73 && offsetY<52){
+        gameOver.innerHTML = "Game Over - Reload to play again"
         obstacle.classList.remove('obstacleAni');
+        
+        audiogo.play();
+        setTimeout(()=>{
+            audio.pause();
+            audiogo.pause();
+        },1000);
     }
+    else if(offsetX<145 && cross){
+        score +=1;
+        updateScore(score);
+        cross=false;
+        setTimeout(()=>{
+            cross=true;
+        },1000);
+
+        setTimeout(()=>{
+        aniDur = parseFloat(window.getComputedStyle(dino,null).getPropertyValue('animation-duration'));
+        newDur = aniDur -0.1;
+        obstacle.style.animationDuration = newDur + 's';
+
+
+        },500);
+
+        }
 
 },100);
+
+function updateScore(score){
+    scoreCont.innerHTML = "Your Score : "+ score;
+}
